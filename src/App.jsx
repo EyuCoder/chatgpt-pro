16252837e283e60cb54c997eca9292411fdba9e6
venom <eyueldaniel0921@gmail.com>
2022-12-23 15:41:36 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-3xl font-bold underline text-center p-10">
+      Hello I'M Evil ChatGPT 😈
+    </div>
+  )
+}
+
+export default App
