9acfd32258156195a514e82556300177b6243bdb
venom <eyueldaniel0921@gmail.com>
2022-12-26 12:46:01 +0300

    changed build tool

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SideBar from './components/SideBar';
+import ChatView from './components/ChatView';
+import { ChatContextProvider } from './context/chatContext';
+
+const App = () => {
+  return (
+    <ChatContextProvider>
+      <div className="flex transition duration-500 ease-in-out">
+        <SideBar />
+        <ChatView />
+      </div>
+    </ChatContextProvider>
+  );
+};
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        "light-white": "#ffffff2b",
+      },
+    },
+  },
+  plugins: [],
+}
