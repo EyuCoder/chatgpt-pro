962c3e5e5f71e0971458af829ba6d9189ec133ad
venom <eyueldaniel0921@gmail.com>
2022-12-26 10:28:10 +0300

    builf error fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from '@rollup/plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    {
+      ...eslint(),
+      enforce: 'pre',
+      apply: 'build',
+    },
+    react()
+  ],
 })
