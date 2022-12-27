4504e5343eea8a19dc9962824d1bd284c8bc614d
venom <eyueldaniel0921@gmail.com>
2022-12-27 08:32:08 +0300

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,8 @@
 import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 
-// console.log(process.env.API_KEY)
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDRC7KlBFFOnvQVovWFqneWp0i4hW-GP5w",
-  authDomain: "chatgpt-9a01b.firebaseapp.com",
-  projectId: "chatgpt-9a01b",
-  storageBucket: "chatgpt-9a01b.appspot.com",
-  messagingSenderId: "420118153863",
-  appId: "1:420118153863:web:224ac6ce9e46d8fd174622",
-  measurementId: "G-RJKQFVZ80E"
+  // your firebase configuration here
 };
 
 // Initialize Firebase
