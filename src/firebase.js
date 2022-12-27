c7535974c048630a565795084e3817434794503a
venom <eyueldaniel0921@gmail.com>
2022-12-27 08:31:46 +0300

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { initializeApp } from 'firebase/app';
 // console.log(process.env.API_KEY)
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: "AIzaSyDRC7KlBFFOnvQVovWFqneWp0i4hW-GP5w",
+  authDomain: "chatgpt-9a01b.firebaseapp.com",
+  projectId: "chatgpt-9a01b",
+  storageBucket: "chatgpt-9a01b.appspot.com",
+  messagingSenderId: "420118153863",
+  appId: "1:420118153863:web:224ac6ce9e46d8fd174622",
+  measurementId: "G-RJKQFVZ80E"
 };
 
 // Initialize Firebase
