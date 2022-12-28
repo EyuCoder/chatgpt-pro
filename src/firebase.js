1a0e77b0a28077fe85f3b8e4e77e8f29909e1483
venom <eyueldaniel0921@gmail.com>
2022-12-28 20:31:34 +0300

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRC7KlBFFOnvQVovWFqneWp0i4hW-GP5w",
-  authDomain: "chatgpt-9a01b.firebaseapp.com",
-  projectId: "chatgpt-9a01b",
-  storageBucket: "chatgpt-9a01b.appspot.com",
-  messagingSenderId: "420118153863",
-  appId: "1:420118153863:web:224ac6ce9e46d8fd174622",
-  measurementId: "G-RJKQFVZ80E"
+  // put your firebase config here
 };
 
 // Initialize Firebase
