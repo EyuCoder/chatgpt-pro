14acdf9214810199a2c79d48d8a30443fe5276f4
venom <eyueldaniel0921@gmail.com>
2023-06-03 10:41:33 +0300

    analytics

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -3,12 +3,12 @@ const GoogleAnalytics = () => {
     <>
       <script
         async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_PUBLIC_GOOGLE_ANALYTICS_KEY}`}></script>
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GOOGLE_ANALYTICS_KEY}`}></script>
       <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', '$
-        {process.env.REACT_PUBLIC_GOOGLE_ANALYTICS_KEY}');
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date()); 
+        gtag('config', '${process.env.REACT_APP_GOOGLE_ANALYTICS_KEY}');`}
       </script>
     </>
   );
