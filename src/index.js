f1eb42c15e1b814cbf35716bde8c86880e7b16cf
venom <eyueldaniel0921@gmail.com>
2023-06-06 14:54:35 +0300

    reactga4

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
-ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY);
+ReactGA.initialize(`${process.env.REACT_APP_GOOGLE_ANALYTICS_KEY}`);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
