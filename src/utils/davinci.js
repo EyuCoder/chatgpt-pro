6d127971c4b34704d00400e8724a64e0fcef8287
eyucoder <eyueldaniel0921@gmail.com>
2023-09-28 15:46:23 +0300

    system prompt

--- src/utils/davinci.js
+++ src/utils/davinci.js
@@ -1,29 +1,31 @@
 import { ConversationChain } from 'langchain/chains';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { ChatPromptTemplate, MessagesPlaceholder } from 'langchain/prompts';
+import {
+  ChatPromptTemplate,
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+  MessagesPlaceholder,
+} from 'langchain/prompts';
 import { BufferMemory } from 'langchain/memory';
 
 const memory = new BufferMemory({
   returnMessages: true,
   memoryKey: 'history',
-  inputKey: 'input',
 });
 
 export const davinci = async (prompt, key) => {
   const chatPrompt = ChatPromptTemplate.fromMessages([
-    [
-      'system',
-      'The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context and always responds in markdown format. If the AI does not know the answer to a question, it truthfully says it does not know.',
-    ],
+    SystemMessagePromptTemplate.fromTemplate(
+      'The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context and always responds in markdown format. If the AI does not know the answer to a question, it truthfully says it does not know.'
+    ),
     new MessagesPlaceholder('history'),
-    ['human', '{input}'],
+    HumanMessagePromptTemplate.fromTemplate('{input}'),
   ]);
   const model = new ChatOpenAI({
     openAIApiKey: key,
     model: 'gpt-3.5-turbo',
     temperature: 0.3,
   });
-  console.log(await memory.loadMemoryVariables({}));
 
   const chain = new ConversationChain({
     memory: memory,
