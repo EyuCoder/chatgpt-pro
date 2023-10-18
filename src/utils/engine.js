0406f6a83f6223f4f09aa299c61e3ba2faebc6c8
Owen <ocolegro@gmail.com>
2023-10-18 11:50:01 -0400

    fix engine

--- src/utils/engine.js
+++ src/utils/engine.js
@@ -17,7 +17,7 @@ const memory = new BufferMemory({
 export const completions = async (prompt, messages, gptVersion) => {
   const openai = new OpenAI({
     apiKey: "",
-    baseURL: "https://api.sciphi.ai:5000/v1",
+    baseURL: "https://api.sciphi.ai/v1",
     dangerouslyAllowBrowser: true,
   });
   // console.log("messages = ", messages);
