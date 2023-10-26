b8e0eb11e2c12b83aa0c3e26ec580c06ba54ed4c
B sh <you@example.com>
2023-10-27 01:25:36 +0200

    title generation added

--- src/utils/engine.js
+++ src/utils/engine.js
@@ -81,3 +81,43 @@ export const regenerate = async (messages, gptVersion) => {
 
   return response_str;
 }
+
+// this function generates a title based on the messages array, and returns the title
+export const generateTitle = async (messages, gptVersion) => {
+  const openai = new OpenAI({
+    apiKey: "",
+    baseURL: "https://api.sciphi.ai/v1",
+    dangerouslyAllowBrowser: true,
+  });
+  // console.log("messages = ", messages);
+
+  let conversation =
+    "### System:\n\You are a title writing assistant.\n";
+
+
+  for (const message of messages) {
+    // console.log(message);
+    if (message.ai) {
+      conversation += `### Response:\n\n${message.text}\n`;
+    } else {
+      conversation += `### Instruction:\n\n${message.text}\n`;
+    }
+  }
+  conversation += "### Instruction:\n\n Summarize the text conversation above with a 3 word title.\n### Response:\n\n";
+
+  // console.log("Fetching completion with conversation = ", conversation);
+
+  const response = await openai.completions.create({
+    prompt: conversation,
+    model: gptVersion,
+    temperature: 0.1,
+    max_tokens: 16_348,
+    // stream: true,
+  });
+
+  let response_str = response.choices[0].text;
+  response_str = response_str.trim();
+  console.log("response_str = ", response_str);
+
+  return response_str;
+}
