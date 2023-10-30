4dc3a0a811560a2f23d6ea38681e0bfce38209ec
B sh <you@example.com>
2023-10-30 22:37:28 +0100

    fixing for remote

--- src/utils/engine.js
+++ src/utils/engine.js
@@ -1,47 +1,26 @@
 import OpenAI from "openai";
 
-export const completions = async (prompt, messages, gptVersion) => {
-  const openai = new OpenAI({
-    apiKey: "",
-    baseURL: "https://api.sciphi.ai/v1",
-    dangerouslyAllowBrowser: true,
-  });
+export const completions = async(prompt, messages, gptVersion = "SciPhi/SciPhi-Self-RAG-Mistral-7B-32k" ) => {
+const response = await fetch('https://chat.sciphi.ai/api/completions', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    prompt: prompt,
+    messages: messages.map(message => ({
+      id: message.id,
+      createdAt: message.createdAt,
+      text: message.text,
+      ai: message.ai,
+    })),
+    gptVersion: gptVersion
+  }),
+});
 
-  
-  let conversation =
-    "### System:\n\nYou are a helpful assistant which thinks step by step to answer user questions.\n";
-
-  for (const message of messages) {
-    // console.log(message);
-    if (message.ai) {
-      conversation += `### Response:\n\n${message.text}\n`;
-    } else {
-      conversation += `### Instruction:\n\n${message.text}\n`;
-    }
-  }
-  conversation += "### Instruction:\n\n" + prompt + "\n### Response:\n\n";
-
-  // console.log("Fetching completion with conversation = ", conversation);
-  const response = await openai.completions.create({
-    prompt: conversation,
-    model: gptVersion,
-    temperature: 0.1,
-    max_tokens: 16_348,
-    // stream: true,
-  });
-  // for await (const completion of response) {
-  //   // handle each completion here, for example, display it to the user
-  //   console.log(completion);
-  // }
-
-  // console.log("response = ", response);
-  let response_str = response.choices[0].text;
-  response_str = response_str.trim();
-  console.log("response_str = ", response_str);
-
-  // console.log("response_str/ =", response_str);
-  return response_str;
-};
+const responseJson = await response.json();
+return responseJson?.response.trim();
+}
 
 // this function removes the last message from messages array, and resends the messages to the engine, the response is then returned
 export const regenerate = async (messages, gptVersion) => {
