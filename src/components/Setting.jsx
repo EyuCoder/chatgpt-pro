d21411c8aa345fb72850fba5c59c559276ed02e4
B sh <you@example.com>
2023-10-30 22:54:50 +0100

    cleaner code

--- src/components/Setting.jsx
+++ src/components/Setting.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { checkApiKey } from "../utils/checkKeys";
 
 import PropTypes from "prop-types";
 
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const AppData = {
+    baseURL: "https://chat.sciphi.ai/api/completions",
+    model: "SciPhi/SciPhi-Self-RAG-Mistral-7B-32k"
+}
