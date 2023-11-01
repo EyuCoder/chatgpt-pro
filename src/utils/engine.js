d0cb65d4a9ee6b74f63e8279c47486693fdc2e82
Owen <ocolegro@gmail.com>
2023-10-31 21:39:52 -0400

    add api

--- src/utils/engine.js
+++ src/utils/engine.js
@@ -1,51 +1,55 @@
-import axios from 'axios';
+import axios from "axios";
 
 import { AppData } from "./constants";
 
-export const completions = async (prompt, messages, gptVersion = AppData.model) => {
-
+export const completions = async (
+  prompt,
+  messages,
+  gptVersion = AppData.model
+) => {
   // print the fetch request as CURL:
   // console.log(`curl -X POST -H "Content-Type: application/json" -d '{"prompt": "${prompt}", "messages": ${JSON.stringify(messages)}, "gptVersion": "${gptVersion}"}' ${AppData.baseURL}`);
 
   try {
     let stringed = JSON.stringify({
       prompt: prompt,
-      messages: messages?.map(message => ({
-        id: message.id,
-        createdAt: message.createdAt,
-        text: message.text,
-        ai: message.ai,
-      })) || [],
-      gptVersion: gptVersion
-    })
+      messages:
+        messages?.map((message) => ({
+          id: message.id,
+          createdAt: message.createdAt,
+          text: message.text,
+          ai: message.ai,
+        })) || [],
+      gptVersion: gptVersion,
+    });
 
     // console.log("stringed = ", stringed);
 
     // const response = await axios.post("https://chat.sciphi.ai/api/completions", stringed);
     // console.log("response = ", response);
 
+    console.log("fetching from AppData.baseURL = ", AppData.baseURL);
     const response = await fetch(AppData.baseURL, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json; charset=utf-8',
+        "Content-Type": "application/json; charset=utf-8",
       },
       body: stringed,
+      // credentials: "include", // include credentials
     });
     console.log("response = ", response);
 
-
     const responseJson = await response.json();
     return responseJson?.response.trim();
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
   }
-}
+};
 
 // this function generates a title based on the messages array, and returns the title
 export const generateTitle = async (messages, gptVersion = AppData.model) => {
-
-  let messageSet = "You are a title writing assistant. Summarize the text conversation below with a 3 word title.\n";
+  let messageSet =
+    "You are a title writing assistant. Summarize the text conversation below with a 3 word title.\n";
 
   console.log("messages = ", messages);
   console.log("messageSet = ", messageSet);
@@ -66,4 +70,4 @@ export const generateTitle = async (messages, gptVersion = AppData.model) => {
   console.log("response_str = ", response);
 
   return response;
-}
+};
